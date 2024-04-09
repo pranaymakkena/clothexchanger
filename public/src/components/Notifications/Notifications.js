c675188077bfe8ab5d426fdc52a9a474a0a184e4
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-09 11:47:00 +0530

    Update Notifications.js

--- public/src/components/Notifications/Notifications.js
+++ public/src/components/Notifications/Notifications.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function Notifications() {
+  return (
+    <div>
+      <h2>Notifications</h2>
+      {/* Display notifications */}
+    </div>
+  );
+}
+
+export default Notifications;
