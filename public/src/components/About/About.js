819ad06f4ede6faa4379d48b075346fcd078a4cb
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-09 11:46:31 +0530

    Update About.js

--- public/src/components/About/About.js
+++ public/src/components/About/About.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      {/* Display information about the app */}
+    </div>
+  );
+}
+
+export default About;
