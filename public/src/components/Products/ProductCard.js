d1e3f0240887aa9fba46d1b5a165bb90fef73017
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-09 11:48:39 +0530

    Update ProductCard.js

--- public/src/components/Products/ProductCard.js
+++ public/src/components/Products/ProductCard.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function ProductCard({ product }) {
+  return (
+    <div className="product-card">
+      <Link to={`/product/${product.id}`}>
+        <img src={product.image} alt={product.name} />
+        <h3>{product.name}</h3>
+      </Link>
+    </div>
+  );
+}
+
+export default ProductCard;
