078a9290bf0b5d1b6e102a22fe48c5d29be3b53a
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-09 11:46:14 +0530

    Update AdminPanel.js

--- public/src/components/Admin/AdminPanel.js
+++ public/src/components/Admin/AdminPanel.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function AdminPanel() {
+  return (
+    <div>
+      <h2>Admin Panel</h2>
+      {/* Display admin functionalities */}
+    </div>
+  );
+}
+
+export default AdminPanel;
