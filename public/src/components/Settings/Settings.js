71435d5866f52f6ebc5f176b6306f0a0aa41e709
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-09 11:46:44 +0530

    Update Settings.js

--- public/src/components/Settings/Settings.js
+++ public/src/components/Settings/Settings.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function Settings() {
+  return (
+    <div>
+      <h2>Settings</h2>
+      {/* Display user settings */}
+    </div>
+  );
+}
+
+export default Settings;
