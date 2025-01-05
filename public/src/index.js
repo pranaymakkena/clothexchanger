9f5fe5d9a23bad336cfcc99a5597e42bce24ab46
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2025-01-05 20:43:10 +0530

    Delete public/src directory

--- public/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'; // Import your index-specific CSS
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
