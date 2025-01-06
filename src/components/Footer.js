129cfd4c0dcc953978c9ae8e836adee526609829
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2025-01-06 11:00:49 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2025 Cloth Exchanger. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
