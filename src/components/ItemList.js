927a095d135eb012255e99a6c1338b3ce7bfcdc9
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2025-01-06 11:16:41 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ItemCard from './ItemCard';
 
 const ItemList = ({ items }) => {
+  if (!items || items.length === 0) {
+    return <p>No items to display!</p>;
+  }
+
   return (
     <div className="item-list">
       {items.map((item, index) => (
