37ed29e4abd4e7e0cea3b9aa9920f822aec0e23a
Pranay Makkena <pranaymakkena@gmail.com>
2025-01-26 21:46:10 +0530

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h1 className="about-title">About Us</h1>
+      <p className="about-description">
+        Our platform was created to promote sustainable fashion and reduce waste. By swapping party wear and occasion-specific outfits, 
+        we help people save money and contribute to a greener planet.
+      </p>
+      <p className="about-mission">
+        Our mission is to build a community that embraces sharing and reusing instead of buying new outfits for every occasion. 
+        Let's make sustainable fashion the norm!
+      </p>
+    </div>
+  );
+}
+
+export default About;
